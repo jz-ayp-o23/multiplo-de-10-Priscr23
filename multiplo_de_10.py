04852eeb65c912974d5d18f888d2618a35b7b8c2
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-11 17:03:32 -0600

    acento

--- multiplo_de_10.py
+++ multiplo_de_10.py
@@ -14,7 +14,7 @@ numero = int(input("Ingrese un número: "))
 
 # Proceso
 if numero % 10 == 0:
-    print(f"El numero {numero} es múltiplo de 10")
+    print(f"El número {numero} es múltiplo de 10")
 
 else:
     print(f"El número {numero} no es múltiplo de 10")
